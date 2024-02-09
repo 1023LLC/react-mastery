013a1b4bd809e699347909434c16ad66e584cb1f
julius <nyerereoffice@gmail.com>
2024-02-09 20:00:10 +0300

    Initial commit

---
+++ react-02/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import RandomColor from './components/random-color'
+
+function App() {
+
+  return (
+   <div className='App'>
+    
+    {/* This is my RandomColor component */}
+    <RandomColor />
+
+   </div>
+  )
+}
+
+export default App
---
+++ react-03/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import StarRating from './components/star-rating'
+
+function App() {
+
+  return (
+   <div className='App'>
+    
+    {/* This is my StarRating component */}
+    <StarRating />
+
+   </div>
+  )
+}
+
+export default App
